25253262050d2915ea07e135907afcdda77468a1
youssef george youssef <youssefgeorge989@gmail.com>
2025-07-05 07:13:55 +0300

    Add files via upload

---
+++ mean_var_std.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def calculate(lst):
+    if len(lst) != 9:
+        raise ValueError("List must contain nine numbers.")
+
+    matrix = np.array(lst).reshape(3, 3)
+
+    dict1 = {
+        'mean': [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().item()],
+        'variance': [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().item()],
+        'standard deviation': [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().item()],
+        'max': [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().item()],
+        'min': [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().item()],
+        'sum': [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().item()]
+    }
+
+    return dict1
+
